6208725caee0c8c0c84f2043acdb32e01d2c2e8a
Vittorio Ragazzini <vittoragazzini@gmail.com>
2022-06-06 15:55:11 +0200

    contatore junit4

--- EsercizioContatore/test/test/ContatoreTest.java
+++ EsercizioContatore/test/test/ContatoreTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import contatore.Contatore;
 
